c97bad5338cedc1c828b21123ff6560b2c8d1da4
radt <radthoc@gmail.com>
2021-06-07 18:37:28 +0200

    Update AllDiscountRulesTest

--- tests/Unit/Rules/AllDiscountRulesTest.php
+++ tests/Unit/Rules/AllDiscountRulesTest.php
@@ -26,10 +26,14 @@ class AllDiscountRulesTest extends TestCase
         $orders = $this->getOrders();
 
         foreach ($orders as $order) {
+            $date = $order[0] ?? '';
+            $size = $order[1] ?? '';
+            $provider = $order[2] ?? '';
+
             $shipmentDiscount->addOrder(
-                $order[0],
-                $order[1],
-                $order[2]
+                $date,
+                $size,
+                $provider
             );
 
             //Rules that don't need state and can process one order at a time
@@ -76,6 +80,7 @@ class AllDiscountRulesTest extends TestCase
             ['2015-03-15', 'S', 'MR'],
             ['2015-03-16', 'S', 'MR'],
             ['2015-03-17', 'S', 'MR'],
+            ['2015-03-29', 'CUSPS'],
         ];
     }
 
@@ -88,175 +93,212 @@ class AllDiscountRulesTest extends TestCase
                 'provider' => 'MR',
                 'price' => 1.50,
                 'discount' => 0.50,
+                'valid' => true,
             ],[
                 'date' => '2015-02-03',
                 'size' => 'L',
                 'provider' => 'LP',
                 'price' => 6.9,
                 'discount' => 0,
+                'valid' => true,
             ],[
                 'date' => '2015-02-05',
                 'size' => 'S',
                 'provider' => 'LP',
                 'price' => 1.50,
                 'discount' => 0,
+                'valid' => true,
             ],[
                 'date' => '2015-02-06',
                 'size' => 'S',
                 'provider' => 'MR',
                 'price' => 1.50,
                 'discount' => 0.50,
+                'valid' => true,
             ],[
                 'date' => '2015-02-06',
                 'size' => 'L',
                 'provider' => 'LP',
                 'price' => 6.9,
                 'discount' => 0,
+                'valid' => true,
             ],[
                 'date' => '2015-02-07',
                 'size' => 'L',
                 'provider' => 'MR',
                 'price' => 4,
                 'discount' => 0,
+                'valid' => true,
             ],[
                 'date' => '2015-02-08',
                 'size' => 'M',
                 'provider' => 'MR',
                 'price' => 3,
                 'discount' => 0,
+                'valid' => true,
             ],[
                 'date' => '2015-02-09',
                 'size' => 'L',
                 'provider' => 'LP',
                 'price' => 0.0,
                 'discount' => 6.9,
+                'valid' => true,
             ],[
                 'date' => '2015-02-10',
                 'size' => 'L',
                 'provider' => 'LP',
                 'price' => 6.9,
                 'discount' => 0,
+                'valid' => true,
             ],[
                 'date' => '2015-02-10',
                 'size' => 'S',
                 'provider' => 'MR',
                 'price' => 1.50,
                 'discount' => 0.50,
+                'valid' => true,
             ],[
                 'date' => '2015-02-13',
                 'size' => 'L',
                 'provider' => 'LP',
                 'price' => 6.9,
                 'discount' => 0,
+                'valid' => true,
             ],[
                 'date' => '2015-02-16',
                 'size' => 'S',
                 'provider' => 'MR',
                 'price' => 1.50,
                 'discount' => 0.50,
+                'valid' => true,
             ],[
                 'date' => '2015-02-16',
                 'size' => 'L',
                 'provider' => 'LP',
                 'price' => 6.9,
                 'discount' => 0,
+                'valid' => true,
             ],[
                 'date' => '2015-02-17',
                 'size' => 'L',
                 'provider' => 'MR',
                 'price' => 4,
                 'discount' => 0,
+                'valid' => true,
             ],[
                 'date' => '2015-02-18',
                 'size' => 'M',
                 'provider' => 'MR',
                 'price' => 3,
                 'discount' => 0,
+                'valid' => true,
             ],[
                 'date' => '2015-03-02',
                 'size' => 'L',
                 'provider' => 'LP',
                 'price' => 6.9,
                 'discount' => 0,
+                'valid' => true,
             ],[
                 'date' => '2015-03-06',
                 'size' => 'L',
                 'provider' => 'LP',
                 'price' => 6.9,
                 'discount' => 0,
+                'valid' => true,
             ],[
                 'date' => '2015-03-07',
                 'size' => 'L',
                 'provider' => 'MR',
                 'price' => 4,
                 'discount' => 0,
+                'valid' => true,
             ],[
                 'date' => '2015-03-08',
                 'size' => 'M',
                 'provider' => 'MR',
                 'price' => 3,
                 'discount' => 0,
+                'valid' => true,
             ],[
                 'date' => '2015-03-09',
                 'size' => 'L',
                 'provider' => 'LP',
                 'price' => 0.0,
                 'discount' => 6.9,
+                'valid' => true,
             ],[
                 'date' => '2015-03-09',
                 'size' => 'S',
                 'provider' => 'MR',
                 'price' => 1.50,
                 'discount' => 0.50,
+                'valid' => true,
             ],[
                 'date' => '2015-03-10',
                 'size' => 'S',
                 'provider' => 'MR',
                 'price' => 1.50,
                 'discount' => 0.50,
+                'valid' => true,
             ],[
                 'date' => '2015-03-12',
                 'size' => 'S',
                 'provider' => 'MR',
                 'price' => 1.50,
                 'discount' => 0.50,
+                'valid' => true,
             ],[
                 'date' => '2015-03-12',
                 'size' => 'S',
                 'provider' => 'MR',
                 'price' => 1.50,
                 'discount' => 0.50,
+                'valid' => true,
             ],[
                 'date' => '2015-03-13',
                 'size' => 'S',
                 'provider' => 'MR',
                 'price' => 1.50,
                 'discount' => 0.50,
+                'valid' => true,
             ],[
                 'date' => '2015-03-14',
                 'size' => 'S',
                 'provider' => 'MR',
                 'price' => 1.50,
                 'discount' => 0.50,
+                'valid' => true,
             ],[
                 'date' => '2015-03-15',
                 'size' => 'S',
                 'provider' => 'MR',
                 'price' => 1.50,
                 'discount' => 0,
+                'valid' => true,
             ],[
                 'date' => '2015-03-16',
                 'size' => 'S',
                 'provider' => 'MR',
                 'price' => 1.50,
                 'discount' => 0,
+                'valid' => true,
             ],[
                 'date' => '2015-03-17',
                 'size' => 'S',
                 'provider' => 'MR',
                 'price' => 1.50,
                 'discount' => 0,
+                'valid' => true,
             ],
+            [
+                'date' => '2015-03-29', 
+                'size' => 'CUSPS',
+                'provider' => '',
+                'price' => 0,
+                'discount' => 0,
+                'valid' => false,
+            ]
         ];
     }
 }
